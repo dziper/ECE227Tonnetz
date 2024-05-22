28197ab0b62ee749aa87b99892aa4dc0e5db5841
dziper <dziper@me.com>
2024-05-22 16:45:09 -0700

    note map

--- tonnetz.py
+++ tonnetz.py
@@ -7,7 +7,8 @@ NOTE_LOOKUP = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
 
 class Tonnetz:
-    def __init__(self, intervals: Tuple[int] = (3, 4, 5), x: int = 10, y: int = 20, start_note=0, start_octave=3):
+    def __init__(self, intervals: Tuple[int, int, int] = (3, 4, 5), x: int = 10, y: int = 20,
+                 start_note=0, start_octave=3):
         self.G = nx.triangular_lattice_graph(x, y)
         pos = nx.get_node_attributes(self.G, "pos")
         self.pos = rotate_positions(pos, 30)
@@ -18,7 +19,9 @@ class Tonnetz:
         nx.draw_networkx_labels(self.G, self.pos, self.notes)
 
     def _compute_notes(self, intervals, start_note, start_octave):
-        self.notes = {name: "A" for name in self.G.nodes()}
+        self.notes: Dict[Tuple[int, int], str] = {}  # Maps note coord to note name
+        self.note_map: Dict[int, List[Tuple[int, int]]] = {}  # Maps note number to list of positions in Graph
+
         curr = start_note
         current_row = 0
         last_start = start_note
@@ -31,9 +34,10 @@ class Tonnetz:
                 else: curr += len(NOTE_LOOKUP) - intervals[2]
                 last_start = curr
 
-            curr_octave = start_octave + math.floor(curr / len(NOTE_LOOKUP))
-            new_note = f"{NOTE_LOOKUP[curr % len(NOTE_LOOKUP)]}{curr_octave}"
-            self.notes[coord] = new_note
+            self.notes[coord] = num_to_note(curr, start_octave)
+            if curr not in self.note_map:
+                self.note_map[curr] = []
+            self.note_map[curr].append(coord)
             curr -= intervals[0]  # Horizontal
 
 
@@ -43,3 +47,9 @@ def rotate_positions(pos, degrees):
         p[0] * cos(rad) - p[1] * sin(rad),
         p[1] * cos(rad) + p[0] * sin(rad)
     ) for coord, p in pos.items()}
+
+
+def num_to_note(note_num: int, start_octave=0) -> str:
+    curr_octave = start_octave + math.floor(note_num / len(NOTE_LOOKUP))
+    return f"{NOTE_LOOKUP[note_num % len(NOTE_LOOKUP)]}{curr_octave}"
+
